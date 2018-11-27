7ff0fb5c4f6e64731db4a119d18df689b4df57a1
jibinmathew691993 <jibinmathew.69@gmail.com>
2018-11-27 17:33:41 +0530

    100% solution for countdiv

--- lesson5/countdiv.py
+++ lesson5/countdiv.py
@@ -21,14 +21,23 @@ expected worst-case space complexity is O(1).
 """
 
 
+# def solution(A, B, K):
+#
+#     num = ((B-1)-A)//K
+#
+#     if B%K == 0:
+#         num+=1
+#     if A%K == 0:
+#         num+=1
+#     return max(num,0)
+#
+# print(solution(4,4,4))
+
+
 def solution(A, B, K):
 
-    num = ((B-1)-A)//K
-
-    if B%K == 0:
+    num = (B//K) - (A//K)
+    if A % K == 0:
         num+=1
-    if A%K == 0:
-        num+=1
-    return max(num,0)
 
-print(solution(4,4,4))
+    return num
